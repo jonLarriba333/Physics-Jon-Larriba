a89b2f2849aa14e4331f707f81d8a9fd9e0d0f35
Jon Larriba <257492421+jonLarriba333@users.noreply.github.com>
2026-04-17 08:31:39 +0200

    Multiple spawners structure completed

--- PhysicsPractiseLarriba/Assets/Scripts/Spawner.cs
+++ PhysicsPractiseLarriba/Assets/Scripts/Spawner.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject robot;
+    public int positionx = 0;
 
+    private Quaternion q;
+    private Vector3 v3 = Vector3.zero;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Object.Instantiate(robot);
+        q = new Quaternion(0, 0, 0, 0); 
+        v3 = new Vector3(positionx, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Object.Instantiate(robot, v3, q);
+        v3.z = v3.z + 1;
     }
 }
